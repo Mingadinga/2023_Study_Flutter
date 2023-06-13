4e76176d26ea3a8ed8ad1d62ef77f58f1b7ec54b
mingadinga <minpearl0826@gmail.com>
2023-06-13 12:20:35 +0900

    UI Challenge - Header, Button Reuse

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 class Player {
   String? name;
@@ -8,23 +8,93 @@ class Player {
 
 void main() {
   var nico = Player(name: 'nico');
-  runApp(App());
+  runApp(const App());
 }
 
 // core widget
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-          centerTitle: false,
-        ),
-        body: Center(
-          child: Text('hello world'),
-        ),
-      ),
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                /** HEADER **/
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          "Hey, Selena",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                /** HEADER **/
+                const SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                      text: 'Transfer',
+                      bgColor: Colors.amber,
+                      textColor: Colors.black,
+                    ),
+                    Button(
+                      text: 'Request',
+                      bgColor: Color(0x0ff1b33b),
+                      textColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
