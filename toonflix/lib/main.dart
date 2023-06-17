209ce56c6461454ebebf9554a7497966da5162a7
mingadinga <minpearl0826@gmail.com>
2023-06-17 15:06:16 +0900

    Webtoon App - Data Fetching

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
+
+import 'screens/webtoon_screen.dart';
 
 void main() {
   runApp(const App());
@@ -11,54 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        colorScheme:
-            ColorScheme.fromSwatch(backgroundColor: const Color(0xFFE7626C)),
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  // 일반적인 경우 필드에서 state 초기화 가능
-
-  // initState : 처음 딱 한번 호출, build보다 먼저 호출됨
-  // 부모 요소에 의존하는 데이터 초기화
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  // dispose : 위젯이 스크린에서 제거될 때 호출, 이벤트 구독 취소 등
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large Title",
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: WebtoonScreen(),
     );
   }
 }
---
+++ toonflix/lib/screens/webtoon_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
+
+class WebtoonScreen extends StatelessWidget {
+  WebtoonScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "오늘의 웹툰s",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 2,
+      ),
+      body: FutureBuilder(
+        future: webtoons, // await
+        builder: (context, snapshot) {
+          // state
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text("Loading...");
+        },
+      ),
+    );
+  }
+}
